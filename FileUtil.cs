1446a68637b251710240e6da6f947c14893f1717
Yung BYUN <yungcheolbyun@gmail.com>
2022-12-01 15:46:07 +0900

    Add files via upload

---
+++ FileUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class FileUtil
+{
+    //해쉬테이블 객체에 있는 내용을 파일로 저장
+    public void SaveToFile(Hashtable babo, String fn)
+    {
+        System.IO.FileStream fs = System.IO.File.Create(fn);
+        BinaryFormatter younghee = new BinaryFormatter();
+        younghee.Serialize(fs, babo);
+        fs.Close();
+    }
+    //파일에 있는 내용을 해쉬테이블로 로드
+    public Hashtable LoadFromFile(String filename)
+    {
+        //파일에 있는 내용을 읽어와서 해쉬테이블에 넣는 코드
+        //LoadFromFile
+        FileStream fs = File.Open(filename,
+                FileMode.Open, FileAccess.Read);
+        BinaryFormatter youngchoon = new BinaryFormatter();
+        Hashtable tmp = (Hashtable)youngchoon.Deserialize(fs);
+        fs.Close();
+        return tmp;
+    }
+}
+
---
+++ HashtableUtil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class HashtableUtil
+{
+    public void Store2Hashtable(TextBox tb, Hashtable ht, MonthCalendar cal)
+    {
+        String imsi = cal.SelectionStart.ToString();
+        imsi = imsi.Remove(10, imsi.Length - 10);
+
+        //tbMemo.Text에 저장되어 있는 것을 해쉬테이블에 저장하자.
+        //
+        ht[imsi] = tb.Text;
+    }
+
+    public void ShowToTextBox(Hashtable ht, TextBox tb, MonthCalendar cal)
+    {
+        //monthCalendar에게 선택된 날짜를 물어보고 그날짜를 tbMemo.Text에 저장
+        String imsi = cal.SelectionStart.ToString();
+        imsi = imsi.Remove(10, imsi.Length - 10);
+
+        tb.Text = (String)ht[imsi];
+    }
+
+}
